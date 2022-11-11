abada3444bafa0335f68ea36078ee0370685a5af
Chaparala <S549701@nwmissouri.edu>
2022-11-10 20:22:16 -0600

    updated findById

--- controllers/Application.js
+++ controllers/Application.js
@@ -55,3 +55,14 @@ exports.Application_view_all_Page = async function (req, res) {
         res.send(`{"error": ${err}}`);
     }
 };
+// for a specific Application. 
+exports.Application_detail = async function(req, res) { 
+    console.log("detail"  + req.params.id) 
+    try { 
+        result = await Application.findById( req.params.id) 
+        res.send(result) 
+    } catch (error) { 
+        res.status(500) 
+        res.send(`{"error": document for id ${req.params.id} not found`); 
+    } 
+}; 
