9bd7b6bf7175ef066c7ef3442ab839adde16947f
Chaparala <S549701@nwmissouri.edu>
2022-11-13 19:33:05 -0600

    updated Create View

--- controllers/Application.js
+++ controllers/Application.js
@@ -112,3 +112,17 @@ exports.Application_view_one_Page = async function (req, res) {
         res.send(`{'error': '${err}'}`);
     }
 }; 
+
+// Handle building the view for creating a Application. 
+// No body, no in path parameter, no query. 
+// Does not need to be async 
+exports.Application_create_Page = function (req, res) {
+    console.log("create view")
+    try {
+        res.render('Applicationcreate', { title: 'Application Create' });
+    }
+    catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+}; 
--- routes/Application.js
+++ routes/Application.js
@@ -5,5 +5,6 @@ const Application_controlers= require('../controllers/Application');
 /* GET home page. */
 router.get('/', Application_controlers.Application_view_all_Page);
 router.get('/detail', Application_controlers.Application_view_one_Page);
+router.get('/create', Application_controlers.Application_create_Page);
 
 module.exports = router;
