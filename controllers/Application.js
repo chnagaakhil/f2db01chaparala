04c52c4d40132779158ed6cdcb5445528b28033e
Chaparala <S549701@nwmissouri.edu>
2022-11-13 18:56:52 -0600

    updated one Page view

--- controllers/Application.js
+++ controllers/Application.js
@@ -98,3 +98,17 @@ exports.Application_delete = async function(req, res) {
         res.send(`{"error": Error deleting ${err}}`); 
     } 
 }; 
+
+// Handle a show one view with id specified by query 
+exports.Application_view_one_Page = async function (req, res) {
+    console.log("single view for id " + req.query.id)
+    try {
+        result = await Application.findById(req.query.id)
+        res.render('Applicationdetail',
+            { title: 'Application Detail', toShow: result });
+    }
+    catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+}; 
