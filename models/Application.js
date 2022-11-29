166eae68b5494e0eea8e7ac0e49d2fb70e5c1af2
Chaparala <S549701@nwmissouri.edu>
2022-11-29 11:01:48 -0600

    updated Part-5

--- models/Application.js
+++ models/Application.js
@@ -1,9 +1,26 @@
 const mongoose = require("mongoose")
 const applicationSchema = mongoose.Schema({
-    App_Name: String,
-    App_Company: String, 
-    App_Size: Number, 
-    App_Rating: Number, 
-    App_Category: String
-})
-module.exports = mongoose.model("Application",applicationSchema)
+    App_Name: {
+        type: String,
+        required: [true, 'Name of the Application cannot be empty']
+    },
+    App_Company: {
+        type: String,
+        required: [true, 'Company of the Application cannot be empty']
+    },
+    App_Size: {
+        type: Number,
+        min: [120, 'Size of Application must be at least 120'],
+        max: 3000
+    },
+    App_Rating: {
+        type: Number,
+        min: [1, 'Rating of Application must be a number in between 1 to 5'],
+        max: 5
+    },
+    App_Category: {
+        type: String,
+        required: [true, 'Category of the Application cannot be empty']
+    }
+});
+module.exports = mongoose.model("Application", applicationSchema)
