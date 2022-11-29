e33a7637debee1f71fce0cf0f628d8e04781aee8
Chaparala <S549701@nwmissouri.edu>
2022-11-29 10:40:15 -0600

    updated Part-4

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var mongoose = require('mongoose');
+var mongodb = require('mongodb');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var Application = require('./models/Application');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
 require('dotenv').config();
 const connectionString = process.env.MONGO_CON
@@ -38,6 +42,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -45,15 +56,24 @@ app.use('/users', usersRouter);
 app.use('/Application', appRouter);
 app.use('/gridbuild', gridbuildRouter);
 app.use('/selector', selectorRouter);
-app.use('/resource',resourceRouter);
+app.use('/resource', resourceRouter);
+
+// passport config 
+// Use the existing connection 
+// The Account model
+var Account = require('./models/account');
+
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -67,28 +87,32 @@ async function recreateDB() {
   // Delete everything
   await Application.deleteMany();
   let instance1 = new
-    Application({"App_Name": "WhatsApp", 
-    "App_Company": "META", "App_Size": 150.8, 
-    "App_Rating": 4.7,"App_Category":"Communication"
-  });  
+    Application({
+      "App_Name": "WhatsApp",
+      "App_Company": "META", "App_Size": 150.8,
+      "App_Rating": 4.7, "App_Category": "Communication"
+    });
   let instance2 = new
-    Application({"App_Name": "LinkedIn", 
-    "App_Company": "LinkedIn Corporation", 
-    "App_Size": 301.6, "App_Rating": 4.2,
-    "App_Category":"Social"
-  });
+    Application({
+      "App_Name": "LinkedIn",
+      "App_Company": "LinkedIn Corporation",
+      "App_Size": 301.6, "App_Rating": 4.2,
+      "App_Category": "Social"
+    });
   let instance3 = new
-    Application({"App_Name": "Canvas Student", 
-    "App_Company": "Instructure Inc.", 
-    "App_Size": 113.1, "App_Rating": 4.7,
-    "App_Category":"Education"
-  });
+    Application({
+      "App_Name": "Canvas Student",
+      "App_Company": "Instructure Inc.",
+      "App_Size": 113.1, "App_Rating": 4.7,
+      "App_Category": "Education"
+    });
   let instance4 = new
-    Application({"App_Name": "Zoom", 
-    "App_Company": "Zoom Video Communications, Inc.", 
-    "App_Size": 159.2, "App_Rating": 4.6,
-    "App_Category":"Business"
-  });
+    Application({
+      "App_Name": "Zoom",
+      "App_Company": "Zoom Video Communications, Inc.",
+      "App_Size": 159.2, "App_Rating": 4.6,
+      "App_Category": "Business"
+    });
   instance1.save(function (err, doc) {
     if (err) return console.error(err);
     console.log("First Application saved")
--- routes/Application.js
+++ routes/Application.js
@@ -2,11 +2,21 @@ var express = require('express');
 var router = express.Router();
 const Application_controlers= require('../controllers/Application'); 
 
+// A little function to check if we have an authorized user and continue on or 
+// redirect to login. 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  }
+
 /* GET home page. */
 router.get('/', Application_controlers.Application_view_all_Page);
 router.get('/detail', Application_controlers.Application_view_one_Page);
 router.get('/create', Application_controlers.Application_create_Page);
-router.get('/update', Application_controlers.Application_update_Page);
+router.get('/update', secured, Application_controlers.Application_update_Page);
 router.get('/delete', Application_controlers.Application_delete_Page);
 
 module.exports = router;
